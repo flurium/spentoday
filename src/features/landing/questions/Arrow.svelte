181e9f1e4135febb9d3c828c6b60ac464e24a23e
LitvinenkoVitalii <reskator95@gmail.com>
2023-09-12 18:13:53 +0300

    ADD/Edit Product design

--- src/features/landing/questions/Arrow.svelte
+++ src/features/landing/questions/Arrow.svelte
@@ -1,9 +1,15 @@
+<script lang="ts">
+  let className: string = ""
+  export { className as class }
+</script>
+
 <svg
   xmlns="http://www.w3.org/2000/svg"
   width="26"
   height="12"
   viewBox="0 0 26 12"
   fill="none"
+  class={className}
 >
   <path d="M1 11L13.6476 1.4L25 11" stroke="black" />
 </svg>
