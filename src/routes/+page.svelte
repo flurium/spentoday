4803a200e6dc417746bc906294a6ed151eb71a30
Roman Koshchei <romankoshchei@gmail.com>
2023-10-01 12:08:22 +0300

    sync

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,9 +19,11 @@
 </svelte:head>
 
 <header class="px-5 py-8 bg-secondary-100">
-  <div class="flex justify-between items-center max-w-screen-xl m-auto ">
-    <nav class="basis-1/4 ">
-      <a class="text-brand-violet font-bold text-lg md:text-2xl" href="/">Spentoday</a>
+  <div class="flex justify-between items-center max-w-screen-xl m-auto">
+    <nav class="basis-1/4">
+      <a class="text-brand-violet font-bold text-lg md:text-2xl" href="/"
+        >Spentoday</a
+      >
     </nav>
 
     <nav class="flex-1 justify-center flex gap-5">
@@ -41,8 +43,7 @@
   </div>
 </header>
 
-
-<main class="min-h-[70vh] bg-secondary-100 ">
+<main class="min-h-[70vh] bg-secondary-100">
   <section class="px-6 pt-10 pb-28 md:pb-36">
     <h1
       class="max-w-screen-xl m-auto text-4xl md:text-8xl lg:text-9xl font-bold
@@ -58,8 +59,9 @@
     </h1>
   </section>
 
-
-  <Partners class="py-0 mb-10 bg-brand-pink flex justify-around md:mb-24 md:py-8" />
+  <Partners
+    class="py-0 mb-10 bg-brand-pink flex justify-around md:mb-24 md:py-8"
+  />
 
   <Benefits />
 
@@ -69,36 +71,48 @@
 
   <Strip />
 
- <section class="bg-brand-violet py-32">
-  <div class="px-4 md:px-6">
-    <div
-      class="bg-brand-dark text-white max-w-screen-xl m-auto p-5 md:p-16 rounded-[3rem]
-      grid grid-cols-3 gap-4"
-    >
-      <h2 class="text-3xl md:text-6xl lg:text-8xl font-bold row-start-1 col-span-3">
-        <span class="px-2 md:px-4 bg-brand-pink text-white rounded-xl">ЗРОБІТЬ КРОК</span>
-      </h2>
-      <h2 class="text-3xl md:text-6xl lg:text-8xl font-bold row-start-2 col-span-3">
-        ДО БІЗНЕСУ РАЗОМ
-      </h2>
-      <h2 class="text-3xl md:text-6xl lg:text-8xl font-bold col-start-1 row-start-3 col-span-3 md:col-start-2">З НАМИ</h2>
-
-       <p class="text-lg  col-start-1 col-span-3 row-start-4 md:col-start-2 md:col-span-1 text-left ">
-        Ми запрошуємо вас випробувати всі можливості платформи абсолютно
-        безкоштовно на 14 днів. Почніть свій шлях до успіху вже сьогодні!
-      </p>
-
+  <section class="bg-brand-violet py-32">
+    <div class="px-4 md:px-6">
       <div
-        class="md:col-start-3 md:row-start-3 row-start-5 mt-1 col-start-2 md:row-span-2 justify-self-end aspect-square
-        w-full h-full grid place-content-center"
+        class="bg-brand-dark text-white max-w-screen-xl m-auto p-5 md:p-16
+      rounded-3xl md:rounded-[3rem] grid grid-cols-3 gap-4"
       >
-        <a href="/login" aria-label="Почни зараз">
-          <StartFreeStar />
-        </a>
+        <h2
+          class="text-3xl md:text-6xl lg:text-8xl font-bold row-start-1 col-span-3"
+        >
+          <span class="px-2 md:px-4 bg-brand-pink text-white rounded-xl"
+            >ЗРОБІТЬ КРОК</span
+          >
+        </h2>
+        <h2
+          class="text-3xl md:text-6xl lg:text-8xl font-bold row-start-2 col-span-3"
+        >
+          ДО БІЗНЕСУ РАЗОМ
+        </h2>
+        <h2
+          class="text-3xl md:text-6xl lg:text-8xl font-bold col-start-1 row-start-3 col-span-3 md:col-start-2"
+        >
+          З НАМИ
+        </h2>
+
+        <p
+          class="text-lg col-start-1 col-span-3 row-start-4 md:col-start-2 md:col-span-1 text-left"
+        >
+          Ми запрошуємо вас випробувати всі можливості платформи абсолютно
+          безкоштовно на 14 днів. Почніть свій шлях до успіху вже сьогодні!
+        </p>
+
+        <div
+          class="md:col-start-3 md:row-start-3 row-start-5 mt-1 col-start-2 md:row-span-2 justify-self-end aspect-square
+        w-full h-full grid place-content-center"
+        >
+          <a href="/login" aria-label="Почни зараз">
+            <StartFreeStar />
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
 
   <Pricing />
 
