da7e8d3de76d2d97b38348e07f4f0ebcff2c1014
Roman Koshchei <romankoshchei@gmail.com>
2023-10-03 10:49:58 +0300

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,13 +9,11 @@
   import StartFreeStar from "$lib/assets/StartFreeStar.svelte"
   import Partners from "$features/landing/Partners.svelte"
 
+  let isMenuOpen = false
 
-    let isMenuOpen = false; 
-    
-    function toggleMenu() {
-      isMenuOpen = !isMenuOpen;
-    }
-  
+  function toggleMenu() {
+    isMenuOpen = !isMenuOpen
+  }
 </script>
 
 <svelte:head>
@@ -34,7 +32,6 @@
       >
     </nav>
 
-    
     <nav class="hidden md:flex flex-1 justify-center gap-5">
       <a class="text-xs md:text-base" href="/#benefits">Переваги</a>
       <a class="text-xs md:text-base" href="/#price">Ціна</a>
@@ -42,44 +39,75 @@
     </nav>
 
     <nav class="basis-1/4 text-right">
-      <a class="hidden md:inline-block px-4 py-2 md:px-8 md:py-3 rounded-full bg-brand-violet text-white text-xs md:text-base"
-       href="/login"
+      <a
+        class="hidden md:inline-block px-4 py-2 md:px-8 md:py-3 rounded-full bg-brand-violet text-white text-xs md:text-base"
+        href="/login"
       >
-      Почати
-       </a>
+        Почати
+      </a>
     </nav>
 
-
-    <button on:click={toggleMenu} type="button" class="md:hidden inline-flex items-center justify-center p-2 w-10 h-10 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-expanded={isMenuOpen}>
+    <button
+      on:click={toggleMenu}
+      class="md:hidden inline-flex items-center justify-center p-2
+      w-10 h-10 text-secondary-500 rounded-lg"
+      aria-expanded={isMenuOpen}
+    >
       <span class="sr-only">Open main menu</span>
-      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+      <svg
+        class="w-5 h-5"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 17 14"
+      >
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M1 1h15M1 7h15M1 13h15"
+        />
       </svg>
     </button>
-    
- 
+
     {#if isMenuOpen}
-    <div class="fixed top-0 left-0 w-full h-full bg-brand-violet z-50 ">
-      <div class="m-8 text-white">
-        <button on:click={toggleMenu} type="button" class="close-button absolute top-5 right-5 text-5xl text-white hover:text-gray-700 focus:outline-none">
-          &times;
-        </button>
-        <div>
-          <h2 class="text-3xl bold pb-3">SPENTODAY</h2>
-          <a href="mailto:support@spentoday.com" class="hover:underline">
-            support@spentoday.com
-          </a>
-          <p class="pt-2">+380 66 727 49 05</p>
-        </div>
-        <div class="text-5xl bold absolute bottom-8">
-        <a class="block pb-6"on:click={toggleMenu} href="/#benefits">Переваги</a>
-        <a class="block pb-6" on:click={toggleMenu} href="/#price">Ціна</a>
-        <a class="block pb-6" on:click={toggleMenu} href="/#faq">FAQ</a>
+      <div class="fixed top-0 left-0 w-full h-full bg-brand-violet z-50">
+        <div class="my-8 mx-5 text-white">
+          <div class="flex justify-between items-start">
+            <div>
+              <h2 class="text-3xl bold pb-3">SPENTODAY</h2>
+              <a href="mailto:support@spentoday.com" class="hover:underline">
+                support@spentoday.com
+              </a>
+            </div>
+            <button
+              on:click={toggleMenu}
+              class="text-5xl text-white p-2 h-10 w-10"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="none"
+              >
+                <path d="M1 1L15 15" stroke="white" />
+                <path d="M15 1L1 15" stroke="white" />
+              </svg>
+            </button>
+          </div>
+          <div class="text-5xl bold absolute bottom-8">
+            <a class="block pb-6" on:click={toggleMenu} href="/login">
+              Почати
+            </a>
+            <a class="block pb-6" on:click={toggleMenu} href="/#benefits">
+              Переваги
+            </a>
+            <a class="block pb-6" on:click={toggleMenu} href="/#price">Ціна</a>
+            <a class="block pb-6" on:click={toggleMenu} href="/#faq">FAQ</a>
+          </div>
         </div>
       </div>
-    </div>
     {/if}
-   
   </div>
 </header>
 
